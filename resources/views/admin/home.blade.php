36573e34ba45aff39509a893d2491ddaa92321aa
Joao Pedro <jp.ssilva@live.com>
2021-02-06 13:42:33 -0300

    Instalando e Configurando o AdminLTE

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,20 @@
-PÁGINA INICIAL ADMIN BETA 
+@extends('adminlte::page')
+
+@section('title', 'Painel de Controle')
+    
+@section('content_header')
+    <h1>Painel Administrativo</h1>    
+@endsection
+
+@section('content')
+    Aqui ficará o conteúdo da página ...    
+@endsection
+
+@section('css')
+    Aqui fica tudo referente a CSS
+@endsection
+
+@section('js')
+    Aqui fica tudo referente a Javascript
+@endsection
+
