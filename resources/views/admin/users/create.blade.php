9e23877a3e3774094b21899015166d364c936b7c
Joao Pedro <jp.ssilva@live.com>
2021-02-09 16:17:36 -0300

    Usuário: Erro no input

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -12,7 +12,10 @@
 
     @if ($errors->any())
         <div class="alert alert-danger">
-            <h4>Ocorreu um erro</h4>
+            <h5>
+                <i class="icon fas fa-ban"></i>
+                Ocorreu um erro
+            </h5>
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{$error}}</li>
@@ -22,53 +25,49 @@
         
     @endif
 
+<div class="card">
+    <div class="card-body">
+        <form action="{{route('users.store')}}" method="post" class="form-horizontal">
+            @csrf
+            <div class="form-group row">            
+                    <label class="col-sm-2 col-form-label">Nome Completo</label>
+                    <div class="col-sm-10">
+                        <input type="text" name="name" value="{{old('name')}}"
+                                class="form-control @error('name') is-invalid @enderror" placeholder="Informe o seu nome" />
+                    </div>            
+            </div> <!--FIM DA DIV NOME COMPLETO -->
+            <div class="form-group row">            
+                    <label class="col-sm-2 col-form-label">E-mail</label>
+                    <div class="col-sm-10">
+                        <input type="email" name="email" value="{{old('email')}}"
+                                class="form-control @error('email') is-invalid @enderror" placeholder="Informe o seu e-mail"/>
+                    </div>            
+            </div> <!--FIM DA DIV E-MAIL -->
+            <div class="form-group row">            
+                    <label class="col-sm-2 col-form-label">Senha</label>
+                    <div class="col-sm-10">
+                        <input type="password" name="password" 
+                                class="form-control @error('password') is-invalid @enderror" placeholder="Insira uma senha"/>
+                    </div>            
+            </div> <!--FIM DA DIV SENHA -->
+            <div class="form-group row">            
+                    <label class="col-sm-2 col-form-label">Confirmação de Senha</label>
+                    <div class="col-sm-10">
+                        <input type="password" name="password_confirmation" 
+                                class="form-control @error('password') is-invalid @enderror" placeholder="Confirme a sua senha"/>
+                    </div>            
+            </div> <!--FIM DA DIV CONFIRMAR SENHA -->
+            <div class="form-group row">            
+                    <label class="col-sm-2 col-form-label"></label>
+                    <div class="col-sm-10">
+                        <input type="submit" value="Cadastrar" class="btn btn-success"/>
+                    </div>            
+            </div> <!--FIM DA DIV CONFIRMAR SENHA -->
+        
 
-    <form action="{{route('users.store')}}" method="post" class="form-horizontal">
-        @csrf
-        <div class="form-group">
-            <div class="row">
-                <label class="col-sm-2 control-label">Nome Completo</label>
-                <div class="col-sm-10">
-                    <input type="text" name="name" value="{{old('name')}}"class="form-control" />
-                </div>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="row">
-                <label class="col-sm-2 control-label">E-mail</label>
-                <div class="col-sm-10">
-                    <input type="email" name="email" value="{{old('email')}}" class="form-control" />
-                </div>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="row">
-                <label class="col-sm-2 control-label">Senha</label>
-                <div class="col-sm-10">
-                    <input type="password" name="password" class="form-control" />
-                </div>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="row">
-                <label class="col-sm-2 control-label">Confirmação da Senha</label>
-                <div class="col-sm-10">
-                    <input type="password" name="password_confirmation" class="form-control" />
-                </div>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="row">
-                <label class="col-sm-2 control-label"></label>
-                <div class="col-sm-10">
-                    <input type="submit" value="Cadastrar" class="btn btn-success" />
-                </div>
-            </div>
-        </div>
-
-    </form>    
-
-
+        </form>    
+    </div>
+</div>
 
 
  
