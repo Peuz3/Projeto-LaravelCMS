a14985bae595ab3309155bf70856163a95470ebf
Joao Pedro <jp.ssilva@live.com>
2021-02-16 15:11:21 -0300

    Dashboard: Estrutura

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,20 +1,81 @@
 @extends('adminlte::page')
 
-@section('title', 'Painel de Controle')
+@section('title', 'Painel Administrativo')
     
 @section('content_header')
-    <h1>Painel Administrativo</h1>    
+    <h1>Dashboard</h1>    
 @endsection
 
 @section('content')
-    Aqui ficará o conteúdo da página ...    
+    <div class="row">
+        <div class="col-md-3">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>999</h3>
+                    <p>Visitantes</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-eye"></i>
+                </div>
+            </div>
+        </div> 
+        <div class="col-md-3">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>999</h3>
+                    <p>Usuários On-line</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-heart"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>999</h3>
+                    <p>Páginas</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-sticky-note"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>999</h3>
+                    <p>Usuários</p>
+                </div>
+                <div class="icon">
+                    <i class="far fa-fw fa-user"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Páginas Mais Visitadas</h3>
+                </div>
+                <div class="card-body">
+                    BETA...
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Sobre o Sistema</h3>
+                </div>
+                <div class="card-body">
+                    BETA...
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
-@section('css')
-    {{-- Aqui fica tudo referente a CSS --}}
-@endsection
-
-@section('js')
-    {{-- Aqui fica tudo referente a Javascript --}}
-@endsection
 
