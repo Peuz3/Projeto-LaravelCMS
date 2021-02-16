9b5b018963e87f9c42ea9b3134a8a29fe9b3b080
Joao Pedro <jp.ssilva@live.com>
2021-02-16 15:41:21 -0300

    Dashboard: Informações

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Page;
+use App\Models\User;
+use App\Models\Visitor;
 use Illuminate\Http\Request;
 
 
@@ -15,6 +18,31 @@ class HomeController extends Controller
     
     public function index()
     {
-        return view('admin.home');
+        $visitsCount = 0;
+        $onlineCount = 0;
+        $pageCount = 0;
+        $userCount = 0;
+
+        //Contagem de Visitantes
+        $visitsCount = Visitor::count();
+
+        //Contagem de Usuários On-line
+        $dateLimit = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+        $onlineList = Visitor::select('ip')->where('date_access', '>=', $dateLimit)->groupBy('ip')->get();
+        $onlineCount = count($onlineList);
+
+        //Contagem de Páginas
+        $pageCount = Page::count();
+
+        //Contagem de Usuários
+        $userCount = User::count();
+
+        return view('admin.home',[
+            'visitsCount' => $visitsCount,
+            'onlineCount' => $onlineCount,
+            'pageCount' => $pageCount,
+            'userCount' => $userCount
+
+        ]);
     }
 }
